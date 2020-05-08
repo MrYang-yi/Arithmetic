578238c28128da3c960c387450ab2a5ca277e55f
Yang <Y1286153790@163.com>
2020-05-08 23:47:43 +0800

    date 5.08

--- src/LeetCode/MyAtoi_08.java
+++ src/LeetCode/MyAtoi_08.java
@@ -1,39 +1,37 @@
 package LeetCode;
 
+import sun.misc.Cleaner;
+
 /**
  * topic:字符串转化为整数
- *
  */
 public class MyAtoi_08 {
     public static int myAtoi(String str) {
-        if (str == null || str.length() == 0) {
+        String s = str.trim();
+        if (s == null || str == "") {
             return 0;
         }
-        str = str.trim();
-        int i = 0;
         int flag = 1;
-        int a = 0;
-        int res = 0;
-        int n = str.length();
-        while (i < n && Character.isDigit(str.charAt(i)) || str.charAt(i) == ' '
-                || str.charAt(i) == '+' || str.charAt(i) == '-') {
-            if (str.charAt(i) == ' ' || str.charAt(i) == '+') {
-                i++;
-                continue;
-            }
-            if (str.charAt(i) == '-') {
-                flag = -1;
-                i++;
-                continue;
-            }
-            a = str.charAt(i) - '0';
-            if (res > Integer.MAX_VALUE / 10) {
-                return flag > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            res = res * 10 + a;
+        int i = 0;
+        if (s.charAt(i) == '+') {
+            i++;
+        } else if (s.charAt(i) == '-') {
+            flag = -1;
             i++;
         }
-        return flag > 0 ? res : -res;
+        long result = 0;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            if (result * flag > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else {
+                if (result * flag < Integer.MIN_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            i++;
+        }
+        return (int) result * flag;
     }
 
     public static void main(String[] args) {
@@ -42,8 +40,8 @@ public class MyAtoi_08 {
         //int p3 = myAtoi("-91283472332");
         //int p4 = myAtoi("3.14159");
         int p5 = myAtoi("");
-        // System.out.println(p1);
-        // System.out.println(p2);
+        //System.out.println(p1);
+        //System.out.println(p2);
         //System.out.println(p3);
         //System.out.println(p4);
         System.out.println(p5);
