5ede82ac11614933d6ad21292ca75c6e00857cef
Yang <Y1286153790@163.com>
2020-05-20 10:21:09 +0800

    date 5.20

---
+++ src/LeetCode/FourSum_18.java
@@ -0,0 +1,86 @@
+package LeetCode;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，
+ * 判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？
+ * 找出所有满足条件且不重复的四元组。
+ * <p>
+ * 注意：
+ * 答案中不可以包含重复的四元组。
+ * 示例：
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ * 满足要求的四元组集合为：
+ * [
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
+ * ]
+ */
+public class FourSum_18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return list;
+        }
+        Arrays.sort(nums);
+        int length = nums.length;
+        for (int k = 0; k < length - 3; k++) {
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;
+            }
+            int min1 = nums[k] + nums[k + 1] + nums[k + 2] + nums[k + 3];
+            if (min1 > target) {
+                break;
+            }
+            int max1 = nums[k] + nums[length - 1] + nums[length - 2] + nums[length - 3];
+            if (max1 < target) {
+                continue;
+            }
+            for (int i = k + 1; i < length - 2; i++) {
+                if (i > k + 1 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                int j = i + 1;
+                int h = length - 1;
+                int min = nums[k] + nums[i] + nums[j] + nums[j + 1];
+                if (min > target) {
+                    continue;
+                }
+                int max = nums[k] + nums[i] + nums[h] + nums[h - 1];
+                if (max < target) {
+                    continue;
+                }
+                while (j < h) {
+                    int curr = nums[k] + nums[i] + nums[j] + nums[h];
+                    if (curr == target) {
+                        list.add(Arrays.asList(nums[k], nums[i], nums[j], nums[h]));
+                        j++;
+                        while (j < h && nums[j] == nums[j - 1]) {
+                            j++;
+                        }
+                        h--;
+                        while (j < h && i < h && nums[h] == nums[h + 1]) {
+                            h--;
+                        }
+                    } else if (curr > target) {
+                        h--;
+                    } else {
+                        j++;
+                    }
+                }
+
+            }
+        }
+        return list;
+}
+    public static void main(String[] args) {
+        List<List<Integer>> list = new FourSum_18().fourSum(new int[]{-1, 0, 1, 2, -1, -4}, -1);
+        System.out.println(list);
+    }
+}
