ff783b858e8827fb65668cd31fed0926e57d30fe
Yang <Y1286153790@163.com>
2020-05-27 20:49:40 +0800

    date 5.27

--- src/Array/maxSubArray2.java
+++ src/Array/maxSubArray2.java
@@ -13,10 +13,10 @@ public class maxSubArray2 {
         int nAll=a[0]; //记录n个数的最大值
         int nEnd=a[0]; //记录n个数是否包含第n个数的最大值
         for (int i = 1; i < a.length; i++) {
-            nEnd=Math.max(nEnd+a[i],a[i]);
-            nAll=Math.max(nEnd,nAll);
-        }
-        return nAll;
+            nEnd=Math.max(nEnd+a[i],a[i]);     //这条语句会出现两个作用
+            nAll=Math.max(nEnd,nAll);          //第一个明显的作用是 nEnd加上一个负数  肯定没有原来大
+        }                    //第二个作用是 如果前面所有的数的和加上这个数 都 没有这个数大  说明前面所有的数的和是负数 也会筛掉
+        return nAll;                   //这种做法有一点不足的是 无法知道最大子数组和的起始和终止位置
     }
     public static void main(String[] args) {
         int a[]={-1,4,-3,6,-7,4,8,15,-7,6};
