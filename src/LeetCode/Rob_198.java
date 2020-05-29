70998e48f7dab0e7f934b0bfed6f33abb3421732
Yang <Y1286153790@163.com>
2020-05-29 19:36:37 +0800

    date 5.29

---
+++ src/LeetCode/Rob_198.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class Rob_198 {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0]; //S0
+        int second = Math.max(nums[0], nums[1]); //S1
+        for (int i = 2; i < length; i++) {
+            int temp = second;    //把S1赋值给temp
+            second = Math.max(first + nums[i], second);  //S2=max（S0+nums[2],second）（2由i控制）
+            first = temp;                                //把temp赋值给first
+        }
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int p = new Rob_198().rob(new int[]{2, 1, 1, 2});
+        System.out.println(p);
+    }
+}
