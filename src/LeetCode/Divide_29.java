b48c8000813c2defbc5e830f0f05770f78633be2
Yang <Y1286153790@163.com>
2020-06-05 20:50:26 +0800

    date 6.05

---
+++ src/LeetCode/Divide_29.java
@@ -0,0 +1,60 @@
+package LeetCode;
+
+/**
+ * 29. 两数相除
+ * 给定两个整数，被除数 a 和除数 b。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 返回被除数 a 除以除数 b 得到的商。
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ * <p>
+ * 示例 1:
+ * 输入: a = 10, b = 3
+ * 输出: 3
+ * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ * <p>
+ * 示例 2:
+ * 输入: a = 7, b = -3
+ * 输出: -2
+ * 解释: 7/-3 = truncate(-2.33333..) = -2
+ * <p>
+ * 提示被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1
+ */
+public class Divide_29 {
+    public int divide(int dividend, int divisor) {
+        if (dividend == 0) return 0;
+        if (divisor == 1) return dividend;
+        if (divisor == -1) {
+            if (dividend > Integer.MIN_VALUE) return -dividend;// 只要不是最小的那个整数，都是直接返回相反数就好啦
+            return Integer.MAX_VALUE;// 是最小的那个，那就返回最大的整数啦
+        }
+        long a = dividend;
+        long b = divisor;
+        int sign = 1;
+        if ((a > 0 && b < 0) || (a < 0 && b > 0)) {
+            sign = -1;
+        }
+        a = a > 0 ? a : -a;
+        b = b > 0 ? b : -b;
+        long res = div(a, b);
+        if (sign > 0) return res > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) res;
+        return (int) -res;
+    }
+
+    int div(long a, long b) {  // 似乎精髓和难点就在于下面这几句
+        if (a < b) return 0;
+        long count = 1;
+        long tb = b; // 在后面的代码中不更新b
+        while ((tb + tb) <= a) {
+            count = count + count; // 最小解翻倍
+            tb = tb + tb; // 当前测试的值也翻倍
+        }
+        return (int) (count + div(a - tb, b));
+    }
+
+
+    public static void main(String[] args) {
+        int p = new Divide_29().divide(-2147483648, 1);
+        System.out.println(p);
+    }
+}
