f8947ae1b6620095cf9408482909a1fc2f2d6a6f
Yang <Y1286153790@163.com>
2020-06-13 15:00:00 +0800

    date 6.13

---
+++ src/Sword_Point_To_Offer/test14_2.java
@@ -0,0 +1,40 @@
+package Sword_Point_To_Offer;
+
+/**
+ * 面试题14- II. 剪绳子 II
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+ * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+ * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * 示例 1：
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * <p>
+ * 示例 2:
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ */
+public class test14_2 {
+    public int cuttingRope(int n) {
+        if (n <= 1) return 0;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        if (n == 4) return 4;
+        int number;
+        long count = 1;
+        while (n >= 5) {
+            count *= 3;
+            count %= 1000000007;
+            n -= 3;
+        }
+        return (int) (count * n % 1000000007);
+    }
+
+    public static void main(String[] args) {
+        int p = new test14_2().cuttingRope(120);
+        System.out.println(p);
+    }
+}
