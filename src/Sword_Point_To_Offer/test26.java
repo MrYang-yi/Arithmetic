33381812f0d438445cf94e94cc529d04f363ed0b
Yang <Y1286153790@163.com>
2020-06-15 10:35:38 +0800

    date 6.15

---
+++ src/Sword_Point_To_Offer/test26.java
@@ -0,0 +1,60 @@
+package Sword_Point_To_Offer;
+
+/**
+ * 面试题26. 树的子结构
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * 例如:
+ * 给定的树 A:
+ *     3
+ *    / \
+ *   4   5
+ *  / \
+ * 1   2
+ * 给定的树 B：
+ * <p>
+ *   4
+ * /  \
+ * 1  2
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 1：
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ * <p>
+ * 示例 2：
+ * 输入：A = [3,4,5,1,2], B = [4,1]
+ * 输出：true
+ */
+public class test26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null)
+                && recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    boolean recur(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(5);
+        TreeNode node3 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(1);
+        TreeNode node7 = new TreeNode(2);
+        root.left=node1;
+        root.right=node2;
+        node1.left=node3;
+        node1.right=node4;
+        node5.left=node6;
+        node5.right=node7;
+        boolean flag=new test26().isSubStructure(root,node5);
+        System.out.println(flag);
+    }
+
+}
