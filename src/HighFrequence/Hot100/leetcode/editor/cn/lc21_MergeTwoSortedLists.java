02eebf08033f10384f83c6a4e9a4e771243a7ea3
yangzibiao1 <yangzibiao1@xiaomi.com>
2022-09-10 00:13:58 +0800

    2022-9-9
    Signed-off-by: yangzibiao1 <yangzibiao1@xiaomi.com>

---
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc21_MergeTwoSortedLists.java
@@ -0,0 +1,53 @@
+package HighFrequence.Hot100.leetcode.editor.cn;
+
+public class lc21_MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new lc21_MergeTwoSortedLists().new Solution();
+        ListNode l1 = ListNodeUtils.getListNodeByArray(new int[]{1, 2, 4});
+        ListNode l2 = ListNodeUtils.getListNodeByArray(new int[]{1, 3, 4});
+        ListNode l3 = solution.mergeTwoLists(l1, l2);
+        ListNodeUtils.sout(l3);
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode dummyHead = new ListNode(-1);
+            ListNode cur = dummyHead;
+            while (l1 != null || l2 != null) {
+                if (l1 == null) {
+                    cur.next = l2;
+                    return dummyHead.next;
+                }
+                if (l2 == null) {
+                    cur.next = l1;
+                    return dummyHead.next;
+                }
+                if (l1.val <= l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                    cur = cur.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                    cur = cur.next;
+                }
+
+            }
+            return dummyHead.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
