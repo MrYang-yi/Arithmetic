56a0ad074d407e465e8eb1c10bdf2ff1608d02af
杨子彪 <yangzibiao@yangMacBook-Pro.local>
2022-09-30 00:25:17 +0800

    2022-9-30
    Signed-off-by: 杨子彪 <yangzibiao@yangMacBook-Pro.local>

--- src/HighFrequence/Hot100/leetcode/editor/cn/lc39_CombinationSum.java
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc39_CombinationSum.java
@@ -1,17 +1,38 @@
 package HighFrequence.Hot100.leetcode.editor.cn;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class lc39_CombinationSum{
+public class lc39_CombinationSum {
     public static void main(String[] args) {
         Solution solution = new lc39_CombinationSum().new Solution();
         System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
     }
 
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-         
-    }
-}
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> ans = new ArrayList<List<Integer>>();
+            List<Integer> combine = new ArrayList<Integer>();
+            dfs(candidates, target, ans, combine, 0);
+            return ans;
+        }
 
+        public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int index) {
+            if (index == candidates.length) {
+                return;
+            }
+            if (target == 0) {
+                ans.add(new ArrayList<Integer>(combine));
+                return;
+            }
+            // 直接跳过
+            dfs(candidates, target, ans, combine, index + 1);
+            // 选择当前数
+            if (target - candidates[index] >= 0) {
+                combine.add(candidates[index]);
+                dfs(candidates, target - candidates[index], ans, combine, index);
+                combine.remove(combine.size() - 1);
+            }
+        }
+    }
 }
--- src/Project/Array/lc40_CombinationSumIi.java
+++ src/Project/Array/lc40_CombinationSumIi.java
@@ -1,19 +1,45 @@
 package Project.Array;
 
-import java.util.List;
+import java.util.*;
 
-public class lc40_CombinationSumIi{
+public class lc40_CombinationSumIi {
     public static void main(String[] args) {
         Solution solution = new lc40_CombinationSumIi().new Solution();
-
+        System.out.println(solution.combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
     }
+    class Solution {
+        List<int[]> freq = new ArrayList<int[]>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> sequence = new ArrayList<Integer>();
 
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            int len = candidates.length;
+            List<List<Integer>> res = new ArrayList<>();
+            if (len == 0) {
+                return res;
+            }
+            Arrays.sort(candidates);
+            Deque<Integer> path = new ArrayDeque<>(len);
+            dfs(candidates, len, 0, target, path, res);
+            return res;
+        }
 
-class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-
+        private void dfs(int[] candidates, int len, int begin, int target, Deque<Integer> path, List<List<Integer>> res) {
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = begin; i < len; i++) {
+                if (target - candidates[i] < 0) {
+                    break;
+                }
+                if (i > begin && candidates[i] == candidates[i - 1]) {
+                    continue;
+                }
+                path.addLast(candidates[i]);
+                dfs(candidates, len, i + 1, target - candidates[i], path, res);
+                path.removeLast();
+            }
+        }
     }
-}
-
-
 }
