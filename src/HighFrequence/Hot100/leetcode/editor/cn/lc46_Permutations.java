42f58847d0202467dfddec480a2bb291fa33bce9
杨子彪 <yangzibiao@yangMacBook-Pro.local>
2022-10-12 23:33:27 +0800

    2022-10-12
    Signed-off-by: 杨子彪 <yangzibiao@yangMacBook-Pro.local>

---
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc46_Permutations.java
@@ -0,0 +1,40 @@
+package HighFrequence.Hot100.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc46_Permutations {
+    public static void main(String[] args) {
+        Solution solution = new lc46_Permutations().new Solution();
+        System.out.println(solution.permute(new int[]{1, 2, 3}));
+    }
+
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> list = new ArrayList<>();
+            List<Integer> result = new ArrayList<>();
+            if (nums.length == 0) {
+                return list;
+            }
+            boolean[] used = new boolean[nums.length];
+            backTrack(list, 0, nums, result, used);
+            return list;
+        }
+
+        private void backTrack(List<List<Integer>> list, int index, int[] nums, List<Integer> result, boolean[] used) {
+            if (index == nums.length) {
+                list.add(new ArrayList<>(result));
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i]) {
+                    continue;
+                }
+                result.add(nums[i]);
+                used[i] = true;
+                backTrack(list, index + 1, nums, result, used);
+                result.remove(result.get(result.size() - 1));
+                used[i] = false;
+            }
+        }
+    }
+}
---
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc47_PermutationsIi.java
@@ -0,0 +1,17 @@
+package HighFrequence.Hot100.leetcode.editor.cn;
+
+public class lc47_PermutationsIi{
+    public static void main(String[] args) {
+        Solution solution = new lc47_PermutationsIi().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
