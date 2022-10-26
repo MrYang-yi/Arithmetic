279a48bd758ca6d9f21aab410267a169811088fa
杨子彪 <yangzibiao@192.168.2.104>
2022-10-27 00:15:51 +0800

    2022-10-26
    Signed-off-by: 杨子彪 <yangzibiao@192.168.2.104>

---
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc64_MinimumPathSum.java
@@ -0,0 +1,27 @@
+package HighFrequence.Hot100.leetcode.editor.cn;
+
+public class lc64_MinimumPathSum {
+    public static void main(String[] args) {
+        Solution solution = new lc64_MinimumPathSum().new Solution();
+        System.out.println(solution.minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}));
+    }
+
+    class Solution {
+        public int minPathSum(int[][] grid) {
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (i == 0 && j == 0) {
+                        continue;
+                    } else if (i == 0) {
+                        grid[i][j] = grid[i][j - 1] + grid[i][j];
+                    } else if (j == 0) {
+                        grid[i][j] = grid[i - 1][j] + grid[i][j];
+                    } else {
+                        grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                    }
+                }
+            }
+            return grid[grid.length - 1][grid[0].length - 1];
+        }
+    }
+}
---
+++ src/HighFrequence/Hot100/leetcode/editor/cn/lc75_SortColors.java
@@ -0,0 +1,31 @@
+package HighFrequence.Hot100.leetcode.editor.cn;
+
+public class lc75_SortColors {
+    public static void main(String[] args) {
+        Solution solution = new lc75_SortColors().new Solution();
+        solution.sortColors(new int[]{2, 0, 2, 1, 1, 0});
+
+    }
+
+    class Solution {
+        public void sortColors(int[] nums) {
+            int p = 0, q = nums.length - 1;
+            for (int i = 0; i <= q; ++i) {
+                if (nums[i] == 0) {
+                    nums[i] = nums[p];
+                    nums[p] = 0;
+                    ++p;
+                }
+                if (nums[i] == 2) {
+                    nums[i] = nums[q];
+                    nums[q] = 2;
+                    --q;
+                    if (nums[i] != 1) {
+                        --i;
+                    }
+                }
+            }
+            return;
+        }
+    }
+}
